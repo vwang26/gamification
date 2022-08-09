73d29adebebefef90f79e32d6668d7f8e6e5787b
tchung18 <tchung18@massmutual.com>
2022-08-09 16:32:18 -0400

    Gamification
    Added target="_blank"

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -19,16 +19,16 @@ const Main = () => {
       <Modal show={show}>
         <Modal.Header onClose={()=>{setShow(false)}} title="Insurance">Insurance</Modal.Header>
         <Modal.Body>
-          <div><a href='https://www.figma.com/exit?url=https%3A%2F%2Fwww.massmutual.com%2Ffinancial-wellness%2Fcalculators%2Flife-insurance-calculator'>Life Insurance Calculator</a></div>
+          <div><a href='https://www.figma.com/exit?url=https%3A%2F%2Fwww.massmutual.com%2Ffinancial-wellness%2Fcalculators%2Flife-insurance-calculator' target="_blank">Life Insurance Calculator</a></div>
           <p>If you’re wondering, “How much life insurance do I need?” then our life insurance calculator can provide you with some quick answers. After you calculate your life insurance estimate, consider contacting a financial professional to discuss your individual situation.</p>
 
-          <div><a href="https://www.figma.com/exit?url=https%3A%2F%2Fwww.massmutual.com%2Ffinancial-wellness%2Fcalculators%2Fdisability-income-calculator">Disability Insurance Calculator</a></div>
+          <div><a href="https://www.figma.com/exit?url=https%3A%2F%2Fwww.massmutual.com%2Ffinancial-wellness%2Fcalculators%2Fdisability-income-calculator" target="_blank">Disability Insurance Calculator</a></div>
           <p>If you were too sick or injured to work, do you have enough income to cover your living expenses? Use the disability insurance calculator to find out.</p>
         
-          <div><a href='https://www.figma.com/exit?url=https%3A%2F%2Fwww.massmutual.com%2Ffinancial-wellness%2Fcalculators%2Fincome-protection-quiz'>Income Protection Quiz</a></div>
+          <div><a href='https://www.figma.com/exit?url=https%3A%2F%2Fwww.massmutual.com%2Ffinancial-wellness%2Fcalculators%2Fincome-protection-quiz' target="_blank">Income Protection Quiz</a></div>
           <p>Test your knowledge of a disability’s effect on finances.</p>
         
-          <div><a href="https://www.massmutual.com/financial-wellness/calculators/modal-apr-calculator">Modal Charge Disclosure and APR Calculator</a></div>
+          <div><a href="https://www.massmutual.com/financial-wellness/calculators/modal-apr-calculator" target="_blank">Modal Charge Disclosure and APR Calculator</a></div>
           <p>Use this calculator to determine the total dollar amount and the annual percentage rate (APR) for any additional charge you may incur if you pay your annual premium in installments.</p>
         </Modal.Body>
       </Modal>
